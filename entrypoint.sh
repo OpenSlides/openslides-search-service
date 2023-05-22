dd01a9aa94e58a794f5a488dbb7b4b5259d815fd
Intevation Common Account <Bastian Rihm brihm@intevation.de>
2023-05-22 17:57:35 +0200

    only check if datastore adress is set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-while ! nc -z "$DATASTORE_WRITER_HOST" "$DATASTORE_WRITER_PORT"; do
-    echo "waiting for $DATASTORE_WRITER_HOST:$DATASTORE_WRITER_PORT"
-    sleep 1
-done
+if [[ $DATASTORE_WRITER_HOST && $DATASTORE_WRITER_PORT ]]; then
+    while ! nc -z "$DATASTORE_WRITER_HOST" "$DATASTORE_WRITER_PORT"; do
+        echo "waiting for $DATASTORE_WRITER_HOST:$DATASTORE_WRITER_PORT"
+        sleep 1
+    done
 
-echo "$DATASTORE_WRITER_HOST:$DATASTORE_WRITER_PORT is available"
+    echo "$DATASTORE_WRITER_HOST:$DATASTORE_WRITER_PORT is available"
+fi
 
 exec "$@"
